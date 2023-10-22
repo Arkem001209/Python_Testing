0a601f8771dd27aae5da1bff1272269b16a07dca
Max Arkell <maxarkell@icloud.com>
2023-10-22 09:43:59 +0100

    complete exercise 2 part 1

--- test.py
+++ test.py
@@ -5,7 +5,16 @@ birth_year = 1998
 age = current_year - birth_year
 print("My age is " + str(age))
 
+#exercise 1
+
 first_name = 'Maximilian'
 middle_name = 'Myles Douglas'
 last_name = 'Arkell'
 print('My full name is ' + first_name + ' ' + middle_name + ' ' + last_name)
+
+#exercise 2
+
+length = 92
+width = 48.8
+area = length * width
+print('area is ' + str(area))
