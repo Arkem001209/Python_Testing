5c2d5414777f7d30780484f8ac7176dbfadd337a
Max Arkell <maxarkell@icloud.com>
2023-10-22 09:49:58 +0100

    correct the comments for first exercises

--- test.py
+++ test.py
@@ -5,14 +5,14 @@ birth_year = 1998
 age = current_year - birth_year
 print("My age is " + str(age))
 
-#exercise 1
+#exercise 2
 
 first_name = 'Maximilian'
 middle_name = 'Myles Douglas'
 last_name = 'Arkell'
 print('My full name is ' + first_name + ' ' + middle_name + ' ' + last_name)
 
-#exercise 2
+#exercise 3
 
 length = 92
 width = 48.8
