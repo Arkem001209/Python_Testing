c88de4f38c019f5033f9dea7538d10884a30fd31
Max Arkell <maxarkell@icloud.com>
2023-10-22 09:12:52 +0100

    Add concatinate string exercise

--- test.py
+++ test.py
@@ -4,3 +4,8 @@ current_year = 2023
 birth_year = 1998
 age = current_year - birth_year
 print("My age is " + str(age))
+
+first_name = 'Maximilian'
+middle_name = 'Myles Douglas'
+last_name = 'Arkell'
+print('My full name is ' + first_name + ' ' + middle_name + ' ' + last_name)
