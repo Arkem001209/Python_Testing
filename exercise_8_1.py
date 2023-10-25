336113e5e588a3dd562eee58b6dc7c8d7be74bec
Max.Arkell <maxarkell@icloud.com>
2023-10-25 14:53:43 +0100

    Add if statements

--- exercise_8_1.py
+++ exercise_8_1.py
@@ -4,3 +4,12 @@ bangladesh = ["dhaka", "khulna", "rangpur"]
 
 city_name = input("Enter a city name: ")
 city_name = str(city_name)
+
+if city_name in india:
+    print("This city is in India!")
+elif city_name in pakistan:
+    print("This city is in Pakistan!")
+elif city_name in bangladesh:
+    print("This city is in Bangladesh!")
+else:
+    print("Sorry I cant identify which area this city is in")
