a041e2aa90a04e7c617b9cef500082b7ff61928e
Max.Arkell <maxarkell@icloud.com>
2023-10-25 14:51:47 +0100

    create input variable and make it a string

--- exercise_8_1.py
+++ exercise_8_1.py
@@ -1,3 +1,6 @@
 india = ["mumbai", "banglore", "chennai", "delhi"]
 pakistan = ["lahore","karachi","islamabad"]
 bangladesh = ["dhaka", "khulna", "rangpur"]
+
+city_name = input("Enter a city name: ")
+city_name = str(city_name)
